e0d3994e0f0ad517f4762f42d0fa6ce2ae3618b4
gad828221 <gadm92266@gmail.com>
2026-05-02 13:17:31 +0300

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,16 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './pages/LoginPage';
+import Home from './pages/Home';
+
 function App() {
-  return <h1 style={{ color: 'white', textAlign: 'center', marginTop: '50px' }}>Hello World - الموقع يعمل</h1>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
