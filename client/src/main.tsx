3ca5b4fe5af347a616d707d4bcd1717703469490
gad828221 <gadm92266@gmail.com>
2026-05-02 12:55:51 +0300

    Update main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,3 +3,12 @@ import App from "./App";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(<App />);
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
+import './index.css';
+
+const root = document.getElementById('root');
+if (root) {
+  createRoot(root).render(<App />);
+}
